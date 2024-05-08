f6102b62dbac6cacf3abe9849e676592d6f6600e
Harsh Prakash <harsh.prakashdps@gmail.com>
2024-05-08 18:27:47 +0530

    Created the filter component

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,62 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { TextField, Autocomplete, MenuItem } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+const names = [
+  "Humaira Sims",
+  "Santiago Solis",
+  "Dawid Floyd",
+  "Mateo Barlow",
+  "Samia Navarro",
+  "Kaden Fields",
+  "Genevieve Watkins",
+  "Mariah Hickman",
+  "Rocco Richardson",
+  "Harris Glenn",
+];
 
-export default function Filter() {
-  return <div>Filter</div>;
+export default function Filter(props) {
+  const dispatch = useDispatch(); // Initialize useDispatch hook
+
+  const handleFilterChange = (event, value) => {
+    dispatch(props.updateFilter(value)); // Dispatch action to update Redux store
+  };
+
+  return (
+    <>
+      <Autocomplete
+        sx={{
+          marginRight: "8px",
+          marginTop: "8px",
+          width: props.size,
+          fontWeight: 500,
+          fontSize: "0.8125rem",
+        }}
+        multiple={props.multiple}
+        options={names}
+        getOptionLabel={(option) => option}
+        disableCloseOnSelect
+        onChange={handleFilterChange}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="outlined"
+            label={props.type}
+            placeholder={props.type}
+          />
+        )}
+        renderOption={(props, option, { selected }) => (
+          <MenuItem
+            key={option}
+            value={option}
+            sx={{ justifyContent: "space-between" }}
+            {...props}
+          >
+            {option}
+            {selected ? <CheckIcon color="info" /> : null}
+          </MenuItem>
+        )}
+      />
+    </>
+  );
 }
