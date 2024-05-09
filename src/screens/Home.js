639c6d8108bcfe7ccfea3141381a97d3ee92909a
Harsh Prakash <harsh.prakashdps@gmail.com>
2024-05-09 15:44:16 +0530

    Added the infinite scroll functionality and dynamic data fetching

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,25 +5,46 @@ import { getSampleJdJSON } from "../assets/sampleJdData";
 
 export default function Home() {
   const [jobDataArray, setJobDataArray] = useState([]);
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const dataArr = getSampleJdJSON().slice(0, 13);
-        console.log(dataArr);
-        setJobDataArray(dataArr); // Update state with fetched data
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setError(error.message);
-        setLoading(false);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const dataArr = getSampleJdJSON().slice((page - 1) * 8, page * 8);
+      console.log(dataArr);
+      setJobDataArray((prev) => [...prev, ...dataArr]); // Update state with fetched data
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error.message);
+      setLoading(false);
+    }
+  };
 
+  // Function to increase the page number on reaching the bottom
+  const handleInfiniteScroll = async () => {
+    try {
+      if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.scrollHeight - 1
+      ) {
+        setPage((prev) => prev + 1);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Once the page changes, data is fetched again
+  useEffect(() => {
     fetchData();
-  }, []); // Empty dependency array ensures useEffect runs only once
+  }, [page]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleInfiniteScroll);
+    return () => window.removeEventListener("scroll", handleInfiniteScroll);
+  }, []);
 
   if (loading) {
     return <div>Loading...</div>;
